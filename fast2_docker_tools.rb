c47aad1c5821f9ad79a07461858b45825271c847
Srdan Kvrgic <srdan@kvrgic.se>
2015-12-29 16:38:48 +0100

    WIP - första scriptet

---
+++ fast2_docker_tools.rb
@@ -0,0 +1,25 @@
+class Fast2DockerTools < Formula
+  desc "Installations och körpaket för att starta och köra docker containers"
+  homepage "http://webdocker:8090/view/Base/job/installationspaket/"
+  url "http://webdocker:8090/view/Base/job/installationspaket/lastSuccessfulBuild/artifact/docker-fast2install.tar.gz"
+  version "0.0.1"
+  sha256 "a44b30d90b1f9e253ea8d3b3c8238cdfd200f52ebef589be061cedf8fcb64ca5"
+
+  def install
+	bin.install "*.sh"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test fast2_docker_tools`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    bin.install "docker_doctor.sh"
+    system "#{bin}/docker_doctor.sh"
+  end
+end
