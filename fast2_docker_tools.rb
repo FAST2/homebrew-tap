5ecfd34c559059f35b981d1a4533c8540ac46325
zoomix <srdan@kvrgic.se>
2016-01-11 13:49:02 +0100

    IMPR: Ny version.
    Uppdaterade docker_doctor.sh så att den fixar routing på OSX

--- fast2_docker_tools.rb
+++ fast2_docker_tools.rb
@@ -2,7 +2,7 @@ class Fast2DockerTools < Formula
   desc "Installations och körpaket för att starta och köra docker containers"
   homepage "http://webdocker:8090/view/Base/job/installationspaket/"
   url "http://webdocker:8090/view/Base/job/installationspaket/lastSuccessfulBuild/artifact/docker-fast2install.tar.gz"
-  version "0.9.1"
+  version "0.9.2"
   sha256 "0e85ed033d4fd7ddb8d6c80b3daa3e8aa3e11ec90eeac639282b6d1cbf749e03"
 
   def install
