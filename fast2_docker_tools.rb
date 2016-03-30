cd0103600562e9530210fc3fa3b2e586999235a5
zoomix <srdan@kvrgic.se>
2016-03-30 11:04:56 +0200

    Updated version

--- fast2_docker_tools.rb
+++ fast2_docker_tools.rb
@@ -2,7 +2,7 @@ class Fast2DockerTools < Formula
   desc "Installations och körpaket för att starta och köra docker containers"
   homepage "http://webdocker:8090/view/Base/job/installationspaket/"
   url "http://webdocker:8090/view/Base/job/installationspaket/lastSuccessfulBuild/artifact/docker-fast2install.tar.gz"
-  version "1.0.X"
+  version "1.1.0"
   sha256 "f40976e4e4c09c8e35a82cb8c6715bb524f42efbef1926590a314ce7a513fe6c"
 
   def install
