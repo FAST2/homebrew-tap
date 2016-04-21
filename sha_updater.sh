172a908eadb658ea0f6592c04faf9a3ec5616389
Srdan Kvrgic <srdan@kvrgic.se>
2016-04-21 20:39:14 +0200

    OK. Så sha updater uppdaterar nu version också

--- sha_updater.sh
+++ sha_updater.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 export SHA=`curl -s http://webdocker:8090/view/Base/job/installationspaket/lastSuccessfulBuild/artifact/docker-fast2install.tar.gz.sha256`
 
-# Lägger in raderna på en viss rad. Funkar inte på max. Go figure.
+# Bumpar minor versionen med 1, ie 1.2.1 -> 1.2.2  0.9 -> 0.10 etc
+export NEWVERSION=$(grep version fast2_docker_tools.rb | awk '{print $2}' | cut -f2 -d'"' | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+
+# Tar bort raden med "version" och lägger in en ny på rad 5. Funkar inte på OSX. Go figure.
+sed -i "/version/d" fast2_docker_tools.rb
+sed -i "5i\ \ version \"$NEWVERSION\"" fast2_docker_tools.rb
+
+
+# Tar bort raden med "sha" och lägger in en ny på rad 6. Funkar inte på OSX. Go figure.
 sed -i "/sha/d" fast2_docker_tools.rb
 sed -i "6i\ \ \sha256 \"$SHA\"" fast2_docker_tools.rb
+
