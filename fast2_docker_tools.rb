9516786576d9c8f249d9746dfc8aad90fa633440
jenkins-fast2-webdocker <root.fast2@gmail.com>
2017-09-26 08:41:19 +0200

    AUTO: Updaterade SHA number och bumpade patch verseionen

--- fast2_docker_tools.rb
+++ fast2_docker_tools.rb
@@ -2,8 +2,8 @@ class Fast2DockerTools < Formula
   desc "Installations och körpaket för att starta och köra docker containers"
   homepage "http://webdocker:8090/view/Base/job/installationspaket-bin/"
   url "http://webdocker:8090/view/Base/job/installationspaket-bin/lastSuccessfulBuild/artifact/docker-fast2install.tar.gz"
-  version "1.2.68"
-  sha256 "6ebb79d35c9917aed2fc895c7a72f750cca2f48d4c6f3c1667543f47391d7675"
+  version "1.2.69"
+  sha256 "570f7d0b3acb0c50c43c0cd7a4274b9ead3f531fbad49a2b7097eeb03b6c72a2"
 
   def install
     bin.install Dir["*.sh"], Dir["templates"]
